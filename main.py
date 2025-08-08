811a74bac5625aaa9d1b8d2d3ad6a9bc91442a9c
Cacapepe2 <cacapepe220@gmail.com>
2025-08-08 14:42:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import pandas as pd
 #importa uma planilha genérica - marco antonio - puxa pelo pandas 
 # puxa as 5 do science, sigla, lat,long - puxa pelo pandas
+# Escolha do nome - não pode estar no Science e as regras são: 
