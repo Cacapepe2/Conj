9bc344d1c7ba086ae7c675da436e0aea4c6a3cb3
lcslro <156127989+lcslro@users.noreply.github.com>
2025-08-08 14:44:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import pandas as pd
 #importa uma planilha genérica - marco antonio - puxa pelo pandas 
 # puxa as 5 do science, sigla, lat,long - puxa pelo pandas
 # Escolha do nome - não pode estar no Science e as regras são: 
+# UF + Primeira letra de cada se tiver 3 nomes e não está contido no Science, se não; UF + primeira letra da primeira palavra, 2 da segunda
