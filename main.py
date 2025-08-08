1645009c85306e06e112054cb323d771a53cb977
Cacapepe2 <cacapepe220@gmail.com>
2025-08-08 15:04:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
-# importa uma planilha genérica - marco antonio - puxa pelo pandas 
-# puxa as 5 do science, sigla, lat,long - puxa pelo pandas
+
 # Escolha do nome - não pode estar no Science e as regras são: 
 # UF + Primeira letra de cada se tiver 3 nomes e não está contido no Science, se não; UF + primeira letra da primeira palavra, 2 da segunda
 generica = pd.read_excel("Macro Automação SOI", sheet_name = "a")
